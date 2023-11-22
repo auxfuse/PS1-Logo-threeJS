30a25df71431b7569a76b6649141d650af195139
Anthony O'Brien <aux.fuse@yahoo.com>
2023-11-22 23:57:14 +0000

    PS1 Blender export to ThreeJS 🕹

---
+++ src/script.js
@@ -0,0 +1,130 @@
+import GUI from 'lil-gui'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({
+    width: 400
+})
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// GLTF loader
+const gltfLoader = new GLTFLoader();
+
+/**
+ * Texture
+ */
+const bakedTexture = textureLoader.load('baked.jpg');
+bakedTexture.flipY = false;
+bakedTexture.colorSpace = THREE.SRGBColorSpace;
+bakedTexture.minFilter = THREE.LinearFilter;
+
+/**
+ * Material
+ */
+
+const bakedMaterial = new THREE.MeshBasicMaterial({
+    map: bakedTexture
+});
+
+/**
+ * Model
+ */
+gltfLoader.load('ps_bake.glb', (gltf) => {
+
+    gltf.scene.traverse((child) => {
+        child.material = bakedMaterial;
+    });
+    scene.position.set(-0.5,-1.5,1);
+    scene.add(gltf.scene);
+
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height, 0.1, 150);
+camera.position.x = 4
+camera.position.y = 4.25
+camera.position.z = 6
+
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true;
+controls.enableZoom = false;
+controls.minPolarAngle = 0;
+controls.maxPolarAngle = Math.PI * 0.4;
+controls.minAzimuthAngle = - Math.PI * 0.75;
+controls.maxAzimuthAngle = Math.PI * 0.75;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
